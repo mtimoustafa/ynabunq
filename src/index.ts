7d490652dae488a39966c4f66ace5f9647dca16b
mtimoustafa <mohamed@springest.com>
2021-11-15 23:06:16 +0100

    Test drive with YNAB API + run TS compiler on start

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,39 @@ require('dotenv').config()
 
 const axios = require('axios').default
 
+interface BudgetDateFormatInput {
+  format: string
+}
+interface BudgetCurrencyFormatInput {
+  iso_code: string
+  example_format: string
+  decimal_digits: number
+  decimal_separator: string
+  symbol_first: boolean
+  group_separator: string
+  currency_symbol: string
+  display_symbol: boolean
+}
+interface BudgetInput {
+  id: string
+  name: string
+  last_modified_on: Date
+  first_month: Date
+  last_month: Date
+  date_format: BudgetDateFormatInput
+  currency_format: BudgetCurrencyFormatInput
+}
+
 const getBudgets = async function() {
-  const { status, data: response } = await axios.get('/v1/budgets', {
+  const { data: response } = await axios.get('/v1/budgets', {
     baseURL: process.env.YNAB_API_PATH,
     headers: {
       'Authorization': `Bearer ${process.env.YNAB_API_TOKEN}`
     }
-  }).catch(error => error.response)
+  })
 
-  console.log(status, response)
+  const budgets: BudgetInput[] = response.data
+  console.log(budgets)
 }
 
 getBudgets()
